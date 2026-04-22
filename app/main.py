86cfeb0bdcd1d4fcdf4772a95ec9fc7f4917715a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2026-04-21 23:30:19 -0300

    Adicionar CORS middleware pra permitir integração com front-end

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.routes.classify import router as classify_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=[True],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(classify_router)
